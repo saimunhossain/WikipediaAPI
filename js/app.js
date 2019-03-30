6592fff9eb3030e1afa103f6f6933462dd212d65
Md Saimun Hossain <saimonanam@gmail.com>
2019-03-30 13:07:49 +0600

    searching data is showing in console with loading icon

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// select elements
+
+const loading = document.querySelector(".loading");
+const searchForm = document.getElementById("searchForm");
+const output = document.querySelector(".output");
+const search = document.getElementById("search");
+const feedback = document.querySelector(".feedback");
+
+const base = 'http://en.wikipedia.org/w/api.php';
+const url = '?action=query&format=json&list=search&srsearch=apple';
+
+searchForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const value = search.value;
+
+    if(value === ""){
+        showFeedback('please enter a valid search value')
+    }else{
+        search.value = "";
+        ajaxWiki(value);
+    }
+});
+
+// Show feedback
+
+function showFeedback(text){
+    feedback.classList.add("showItem");
+    feedback.innerHTML = `<p>${text}</p>`;
+
+    setTimeout(()=>feedback.classList.remove("showItem"), 3000);
+}
+
+// ajax wiki
+
+function ajaxWiki(search){
+    output.innerHTML = "";
+    loading.classList.add('showItem');
+    console.log(search);
+    
+}
